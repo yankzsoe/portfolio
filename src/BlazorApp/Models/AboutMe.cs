964e2cbe7bffe5d7ce2f76603437ff599ef17937
Yayang Suryana <suryana@ebconnect.com>
2026-05-24 17:10:16 +0700

    enhance AboutMe
    upgrade NET version from 7.0 to 8.0

--- src/BlazorApp/Models/AboutMe.cs
+++ src/BlazorApp/Models/AboutMe.cs
@@ -3,6 +3,7 @@ namespace BlazorApp.Models;
 public class AboutMe
 {
     public string Description { get; set; } = string.Empty;
+    public int? ExperienceStartYear { get; set; }
     public List<string> Skills { get; set; } = new();
     public string DetailOrQuote { get; set; } = string.Empty;
 }
